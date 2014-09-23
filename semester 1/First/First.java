53cf0459c74c03515b26bbda89e3db28f4602a9a
unknown <x14404242@scr3-068.student.nci.col>
2014-09-23 11:17:17 +0100

    first commit

---
+++ semester 1/First/First.java
@@ -0,0 +1,7 @@
+public class First{
+    public static void main(String arg[]){
+      System.out.println("mickey mouse");
+      System.out.println("This is my first");
+      System.out.println("java program");
+        }
+      }
